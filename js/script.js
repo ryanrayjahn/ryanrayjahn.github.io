3d20af1406b149a5e86950c0252463149d4ce7ee
Ryan Odum <ryanrayjahn3@gmail.com>
2017-07-24 09:04:58 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ var home = "index.html";
 var resume = "resume.html";
 var profile = "profile.html";
 var locationLength = location.href.length;
-
+var x = "Total Width: " + screen.width;
+var y = "Total Width: " + screen.availWidth;
+var z = document.getElementsByTagName("body").width;
 
 if (location.href.substring(locationLength - home.length, locationLength) == home){
 alert("Welcome to my Website!");
@@ -15,8 +17,10 @@ alert("Welcome to my resume page!");
 
 else if (location.href.substring(locationLength - profile.length, locationLength) == profile){
 alert("Welcome to my profile page!");
+alert(x);
+alert(y);
+alert(z);
 }
-
 else {
 
 }

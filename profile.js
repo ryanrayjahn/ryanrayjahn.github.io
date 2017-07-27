962a59604a59637d17adbeba98b577e7cf67407c
Ryan Odum <ryanrayjahn3@gmail.com>
2017-07-27 09:04:39 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchbox");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td1 = tr[i].getElementsByTagName("td")[0];
+    td2 = tr[i].getElementsByTagName("td")[1];
+    td3 = tr[i].getElementsByTagName("td")[2];
+    td4 = tr[i].getElementsByTagName("td")[3];
+    if (td1) {
+      if (td3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
